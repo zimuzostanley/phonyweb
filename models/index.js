72e4d0aa7515bd456a5ec61b7a4052ed15eebd24
Zim <zim@gmail.com>
2013-12-19 00:01:08 +0100

    Still on everyauth

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 if (process.env.MONGOHQ_URL) {
-	mongoose.connect(process.env.MONGOHQ_URL);
+	console.log(process.env.MONGOHQ_URL);
+	mongoose.connect('mongodb://heroku:8fe0e3b4ff8b4d35236a02ebf8348753@paulo.mongohq.com:10009/app20466087');
 }
 else {
 	mongoose.connect('mongodb://local.host/phonyweb');
