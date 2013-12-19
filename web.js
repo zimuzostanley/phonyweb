4fda941f8af42d2d7caafbf5d9adce63c682d370
Zim <zim@gmail.com>
2013-12-19 01:22:55 +0100

    Still on everyauth, trying to get sessions working on heroku

--- web.js
+++ web.js
@@ -49,12 +49,12 @@ app.configure('development', function() {
 	//app.use(express.logger());
 	app.use(express.favicon());
 	app.use(express.cookieParser('My mothers maiden name'));
-	// app.use(express.session({
-	// 	store: new MongoStore({
-	// 		url: 'mongodb://localhost/phonyweb'
-	// 	}),
-	// 	secret: 'My mothers maiden name'
-	// }));
+	app.use(express.session({
+		store: new MongoStore({
+			url: process.env.MONGOHQ_URL || 'mongodb://localhost/phonyweb'
+		}),
+		secret: 'My mothers maiden name'
+	}));
 	app.use(everyauth.middleware());
 	app.use(app.router);
 	app.use(express.favicon(path.join(__dirname, 'public/img/favicon.jpg')));
